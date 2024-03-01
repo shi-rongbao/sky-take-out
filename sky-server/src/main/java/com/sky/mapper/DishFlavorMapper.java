26f8e7ee23c87a5fcc3f3bbe96ebad3ccf62669b
ShiRongbao <2502260933@qq.com>
2024-03-02 01:00:04 +0800

    菜品分页查询代码开发

--- sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
@@ -1,6 +1,9 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
+import com.sky.dto.DishPageQueryDTO;
 import com.sky.entity.DishFlavor;
+import com.sky.vo.DishVO;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -18,4 +21,11 @@ public interface DishFlavorMapper {
      * @param flavors 装有所有口味的List集合
      */
     void insertBatch(List<DishFlavor> flavors);
+
+    /**
+     * 菜品分页查询
+     * @param dishPageQueryDTO 菜品分页查询所需要的参数
+     * @return 返回Page集合
+     */
+    Page<DishVO> pageQuery(DishPageQueryDTO dishPageQueryDTO);
 }
--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -1,6 +1,8 @@
 package com.sky.service;
 
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
 import org.springframework.stereotype.Service;
 
 /**
@@ -15,4 +17,12 @@ public interface DishService {
      * @param dishDTO 菜品所封装成的对象
      */
     void saveWithFlavor(DishDTO dishDTO);
+
+    /**
+     * 菜品分页查询
+     * @param dishPageQueryDTO 菜品分页查询所需要的参数
+     * @return 返回PageResult
+     */
+    PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO);
+
 }
